414bf74b96896bf1981c60d495577227d72cc191
Dilmer Valecillos <dilmervst@gmail.com>
2019-04-07 02:34:39 -0600

    Exposed properties added to meteor VisualEffect

---
+++ Assets/Scripts/VFXController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFXController : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect visualEffect;
+
+    [SerializeField, Range(0, 5)]
+    private float meteorIntensity = 0;
+
+    [SerializeField]
+    private Gradient meteorGradient = null;
+
+    [SerializeField, Range(0, 0.5f)]
+    private float meteorSize = 0;
+
+    void Update()
+    {
+        visualEffect.SetFloat("MeteorIntensity", meteorIntensity);
+        visualEffect.SetFloat("MeteorSize", meteorSize);
+        visualEffect.SetGradient("MeteorGradient", meteorGradient);
+    }
+}
